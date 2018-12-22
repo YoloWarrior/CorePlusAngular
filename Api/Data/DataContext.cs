cf57b92798094ef7c769a0b1b9a22cbd8a9701f0
Yolo <tema-sid@mail.ru>
2018-12-22 20:43:40 +0300

    Добавьте файлы проекта.

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using CorePlusAngular.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorePlusAngular.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
