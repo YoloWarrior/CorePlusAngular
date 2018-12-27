2de1d778e2b53d98811ac3bce3497319fafe1647
Yolo <tema-sid@mail.ru>
2018-12-28 01:22:36 +0300

    fix,re-creationSpa

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ValueComponent} from './Value/value.component'
-
+import {ValueComponent} from './Value/value.component';
 @NgModule({
   declarations: [
     AppComponent,
--- Spa/src/app/Value/value.component.ts
+++ Spa/src/app/Value/value.component.ts
@@ -1,19 +1,19 @@
 import {Component,OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http';
 @Component({
 	selector:'app-value',
-	templateUrl:'./value.component.html',
-	styleUrls:['./value.component.css']
+	templateUrl:'./value.component.html'
 })
 export class ValueComponent implements OnInit {
+	
 	values:any;
-	constructor(private http:HttpClient) {
+	constructor(private http: HttpClient) {
 		// code...
 	}
-	ngOnInit(){this.getValues()}
+	ngOnInit(){this.getValues();}
 
 	getValues(){
-		this.http.get("http://localhost:5000/api/values").subscribe(response=>{
+		this.http.get('http://localhost:5000/api/values').subscribe(response=>{
 			this.values = response;
 		},error =>{
 			console.log(error);
